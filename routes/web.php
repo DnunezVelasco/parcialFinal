99f43fb94b403dcb49b3e451e08cc65faa44ee19
David Alejandro Nuñez Velasco <semillerocecep@gmail.com>
2023-11-23 10:47:37 -0500

    Configuración de rutas principales

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Carrera\CarreraController;
+use App\Http\Controllers\Estudiantes\EstudianteController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,18 +17,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/',[PublicController::class,'index'])->name('public.index');
+
+// Route::get('/index', function () {
+//     return view('index');
+// })->middleware(['auth', 'verified'])->name('index');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/estudiantes', EstudianteController::class);
+    Route::resource('/carreras', CarreraController::class);
 });
 
 require __DIR__.'/auth.php';
+require __DIR__.'/api.php';
